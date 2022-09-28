573775e008879886e6dc8509104f3e0bfc8af66e
kizato1018 <kizato1018@gmail.com>
2022-09-28 15:50:54 +0800

    Title Scene v1.0

---
+++ Assets/Script/TitleBottomScript.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitleBottomScript : MonoBehaviour
+{
+    public void ContinueBottom()
+    {
+    }
+    public void StartBottom()
+    {
+        SceneManager.LoadScene(1);
+    }
+    public void SettingBottom()
+    {
+    }
+    public void ExitBottom()
+    {
+        Debug.Log("Exit");
+        Application.Quit();
+    }
+}
